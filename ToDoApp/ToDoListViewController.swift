c5873e8c9846766c1be4aa04299ce986a34c3819
MityaAndreevich <dmitry.logachev@icloud.com>
2022-02-10 14:58:16 +0300

    add table view datasource methods

--- ToDoApp/ToDoListViewController.swift
+++ ToDoApp/ToDoListViewController.swift
@@ -9,16 +9,29 @@ import UIKit
 
 class ToDoListViewController: UITableViewController{
     
+    let itemArray = ["Find 10000$", "Buy Eggs", "Buy New G-Wagen"]
+    
     override func viewWillAppear(_ animated: Bool) {
         setNavBar()
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //MARK: - TableView Data Source Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = itemArray[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
+    }
 }
 
 extension ToDoListViewController {
