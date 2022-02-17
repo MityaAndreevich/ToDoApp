7d3b42fc92943784785de34d82a3eb5138905547
MityaAndreevich <dmitry.logachev@icloud.com>
2022-02-17 21:22:22 +0300

    reorganise due to MVC create new model for items

---
+++ ToDoApp/Data Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  ToDoApp
+//
+//  Created by Dmitry Logachev on 17.02.2022.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
