d2d886fb99f0d0e1b76beecbdaba9b5a5a1cc758
MityaAndreevich <dmitry.logachev@icloud.com>
2022-07-31 19:53:02 +0300

    deleted undone methods

--- ToDoApp/Data Model/Category.swift
+++ ToDoApp/Data Model/Category.swift
@@ -10,5 +10,6 @@ import RealmSwift
 class Category: Object {
     // in latest versions @Persisted is used instead of old variants with @objc and dynamic
     @Persisted var name: String = ""
+    //@Persisted var color: String = ""
     @Persisted var items = List<Item>()
 }
